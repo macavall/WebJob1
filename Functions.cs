d54045e9011b64fd3cd9c456cd9a75e075ce5a64
Matthew Cavallo <macavall@microsoft.com>
2023-11-08 19:10:54 -0600

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("myqueue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob1
+{
+    public class Program
+    {
+        public static async  Task Main()
+        {
+            var builder = new HostBuilder();
+
+            // Increases the polling frequency to storage
+            builder.UseEnvironment(EnvironmentName.Development);
+
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+            });
+
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+
+            // Similar to Function App's, huh?
+        }
+    }
+}
