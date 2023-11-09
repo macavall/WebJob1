eb813dd9c0e0e3d2e0fed1eb500bbfcb49de5f76
Matthew Cavallo <macavall@microsoft.com>
2023-11-08 19:39:48 -0600

    AppInsights and Graceful shutdown

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,8 +8,23 @@ namespace WebJob1
 {
     public class Program
     {
+        private static bool _running = true;
+        private static string _shutdownFile;
+
         public static async  Task Main()
         {
+            // Get the shutdown file path from the environment
+            _shutdownFile = Environment.GetEnvironmentVariable("WEBJOBS_SHUTDOWN_FILE");
+
+            // Setup a file system watcher on that file's directory to know when the file is created
+            var fileSystemWatcher = new FileSystemWatcher(Path.GetDirectoryName(_shutdownFile));
+            fileSystemWatcher.Created += OnChanged;
+            fileSystemWatcher.Changed += OnChanged;
+            fileSystemWatcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.FileName | NotifyFilters.LastWrite;
+            fileSystemWatcher.IncludeSubdirectories = false;
+            fileSystemWatcher.EnableRaisingEvents = true;
+
+
             var builder = new HostBuilder();
 
             // Increases the polling frequency to storage
@@ -23,6 +39,13 @@ namespace WebJob1
             builder.ConfigureLogging((context, b) =>
             {
                 b.AddConsole();
+
+                // If the key exists in settings, use it to enable Application Insights.
+                string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(instrumentationKey))
+                {
+                    b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+                }
             });
 
             var host = builder.Build();
@@ -33,6 +56,15 @@ namespace WebJob1
             }
 
             // Similar to Function App's, huh?
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.FullPath.IndexOf(Path.GetFileName(_shutdownFile), StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                // Found the file mark this WebJob as finished
+                _running = false;
+            }
         }
     }
 }
